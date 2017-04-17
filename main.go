19598c1edf27ffecf47bef9b2c26aa1804ebc9cf
Pratheek Adidela <apratheek@unaxiom.com>
2017-04-17 15:41:54 +0530

    Added methods that can print to a buffer.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package ulogger
 
 import (
+	"bytes"
+	"fmt"
 	"time"
 )
 
@@ -11,8 +13,10 @@ func New() *Logger {
 }
 
 // Info displays a non-critical log message
-func (*Logger) Info(args interface{}) {
-	infoColor.Print(time.Now().Format("02-01-06 03:04:05"), "\t", args)
+func (log *Logger) Info(args interface{}) {
+	// infoColor.Print(time.Now().Format("02-01-06 03:04:05"), "\t", args)
+	// infoColor.Fprintf()
+	infoColor.Print(log.writeToPrint(args))
 }
 
 // Infoln displays a non-critical log message
@@ -23,6 +27,20 @@ func (*Logger) Infoln(args interface{}) {
 // Debug displays a debugging message useful in development environment
 func (*Logger) Debug(args interface{}) {
 	debugColor.Println(args)
+}
+
+// writeToPrint writes the logging message to a string and returns it
+func (log *Logger) writeToPrint(args interface{}) string {
+	buf := new(bytes.Buffer)
+	fmt.Fprint(buf, time.Now().Format("02-01-06 03:04:05"), "\t", args)
+	return buf.String()
+}
+
+// writeToPrintf writes the logging message to a string and returns it
+func (log *Logger) writeToPrintf(format string, args ...interface{}) string {
+	buf := new(bytes.Buffer)
+	fmt.Fprintf(buf, format, time.Now().Format("02-01-06 03:04:05"), "\t", args)
+	return buf.String()
 }
 
 // WithFields adds the passed fields and attaches them to the logging object
