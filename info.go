47eb1b454007db280dd7fc09cdf99acedf06e462
Pratheek Adidela <apratheek@unaxiom.com>
2017-04-18 20:29:37 +0530

    Overlapping code has been to writes.go, and consequently, code bloat has been reduced in info.go

--- info.go
+++ info.go
@@ -3,7 +3,6 @@ package ulogger
 import (
 	"bytes"
 	"fmt"
-	"strings"
 )
 
 // Info displays a non-fatal log message
@@ -13,26 +12,7 @@ func (log *Logger) Info(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := infoPrefix(log)
-		log.InfoColor.Fprint(buf, args...)
-		log.InfoColor.Print(buf.String())
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprint(buf, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				// Send the actual message here
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
-		ch <- 1
+		write(infoPrefix, log, log.InfoColor, ch, args...)
 	}(ch)
 	<-ch
 }
@@ -44,26 +24,7 @@ func (log *Logger) Infof(format string, args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := infoPrefix(log)
-		log.InfoColor.Fprintf(buf, format, args...)
-		// Using Print instead of Printf, since the format string would be taken into account in the Fprintf method
-		log.InfoColor.Print(buf.String())
-
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprintf(buf, format, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
+		writef(infoPrefix, log, log.InfoColor, ch, format, args...)
 	}(ch)
 	<-ch
 }
@@ -75,25 +36,7 @@ func (log *Logger) Infoln(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := infoPrefix(log)
-		log.InfoColor.Fprint(buf, args...)
-		log.InfoColor.Println(buf.String())
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprintln(buf, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				// Send the actual message here
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
+		writeln(infoPrefix, log, log.InfoColor, ch, args...)
 	}(ch)
 	<-ch
 }
@@ -107,6 +50,5 @@ func infoPrefix(log *Logger) (*bytes.Buffer, logMessage) {
 	fmt.Fprint(buf, " ")
 	log.InfoMessageTypeColor.Fprint(buf, logStruct.MessageType)
 	fmt.Fprint(buf, " ")
-
 	return buf, logStruct
 }
---
+++ writes.go
@@ -0,0 +1,80 @@
+package ulogger
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+type prefixerSignature func(log *Logger) (*bytes.Buffer, logMessage)
+
+// write is applicable for all simple logs
+func write(prefixFunc prefixerSignature, log *Logger, clr *color.Color, ch chan int, args ...interface{}) {
+	// Create the log that needs to be displayed on stdout
+	buf, logStruct := prefixFunc(log)
+	clr.Fprint(buf, args...)
+	clr.Print(buf.String())
+	go func() {
+		// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
+		if !log.RemoteAvailable {
+			ch <- 1
+			return
+		}
+		buf.Reset()
+		fmt.Fprint(buf, args...)
+		logStruct.MessageContent = strings.TrimSpace(buf.String())
+		go func() {
+			// Send the actual message here
+			sendLogMessage(logStruct)
+			ch <- 1
+		}()
+	}()
+}
+
+// writef is applicable for all logs that need to be formatted
+func writef(prefixFunc prefixerSignature, log *Logger, clr *color.Color, ch chan int, format string, args ...interface{}) {
+	// Create the log that needs to be displayed on stdout
+	buf, logStruct := prefixFunc(log)
+	clr.Fprintf(buf, format, args...)
+	clr.Print(buf.String())
+	go func() {
+		// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
+		if !log.RemoteAvailable {
+			ch <- 1
+			return
+		}
+		buf.Reset()
+		fmt.Fprintf(buf, format, args...)
+		logStruct.MessageContent = strings.TrimSpace(buf.String())
+		go func() {
+			// Send the actual message here
+			sendLogMessage(logStruct)
+			ch <- 1
+		}()
+	}()
+}
+
+// writeln is applicable for all logs ending with 'ln'
+func writeln(prefixFunc prefixerSignature, log *Logger, clr *color.Color, ch chan int, args ...interface{}) {
+	// Create the log that needs to be displayed on stdout
+	buf, logStruct := prefixFunc(log)
+	clr.Fprint(buf, args...)
+	clr.Println(buf.String())
+	go func() {
+		// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
+		if !log.RemoteAvailable {
+			ch <- 1
+			return
+		}
+		buf.Reset()
+		fmt.Fprintln(buf, args...)
+		logStruct.MessageContent = strings.TrimSpace(buf.String())
+		go func() {
+			// Send the actual message here
+			sendLogMessage(logStruct)
+			ch <- 1
+		}()
+	}()
+}
