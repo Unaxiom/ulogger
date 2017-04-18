03fb8ea96c20702970d20fea243cec0823682f11
Pratheek Adidela <apratheek@unaxiom.com>
2017-04-18 20:43:13 +0530

    In fatal.go, redundant code has been removed.

--- fatal.go
+++ fatal.go
@@ -3,8 +3,6 @@ package ulogger
 import (
 	"bytes"
 	"fmt"
-	"os"
-	"strings"
 )
 
 // Fatal displays a message and crashes the program
@@ -14,28 +12,7 @@ func (log *Logger) Fatal(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := fatalPrefix(log)
-		log.FatalColor.Fprint(buf, args...)
-		log.FatalColor.Print(buf.String())
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				os.Exit(1)
-				return
-			}
-			buf.Reset()
-			fmt.Fprint(buf, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				// Send the actual message here
-				sendLogMessage(logStruct)
-				os.Exit(1)
-				ch <- 1
-			}()
-		}()
-		ch <- 1
+		write(fatalPrefix, log, log.FatalColor, ch, args...)
 	}(ch)
 	<-ch
 }
@@ -47,27 +24,7 @@ func (log *Logger) Fatalf(format string, args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := fatalPrefix(log)
-		log.FatalColor.Fprintf(buf, format, args...)
-		// Using Print instead of Printf, since the format string would be taken into account in the Fprintf method
-		log.FatalColor.Print(buf.String())
-
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				os.Exit(1)
-			}
-			buf.Reset()
-			fmt.Fprintf(buf, format, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				sendLogMessage(logStruct)
-				ch <- 1
-				os.Exit(1)
-			}()
-		}()
+		writef(fatalPrefix, log, log.FatalColor, ch, format, args...)
 	}(ch)
 	<-ch
 }
@@ -79,25 +36,7 @@ func (log *Logger) Fatalln(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := fatalPrefix(log)
-		log.FatalColor.Fprint(buf, args...)
-		log.FatalColor.Println(buf.String())
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprintln(buf, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				// Send the actual message here
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
+		writeln(fatalPrefix, log, log.FatalColor, ch, args...)
 	}(ch)
 	<-ch
 }
@@ -111,6 +50,5 @@ func fatalPrefix(log *Logger) (*bytes.Buffer, logMessage) {
 	fmt.Fprint(buf, " ")
 	log.FatalMessageTypeColor.Fprint(buf, logStruct.MessageType)
 	fmt.Fprint(buf, " ")
-
 	return buf, logStruct
 }
