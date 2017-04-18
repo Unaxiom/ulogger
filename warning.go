1efb925b97b5f458495a39d4f9fdc2fab4b0972a
Pratheek Adidela <apratheek@unaxiom.com>
2017-04-18 20:48:24 +0530

    Code in warning.go has been moved to the new API.

--- warning.go
+++ warning.go
@@ -3,7 +3,6 @@ package ulogger
 import (
 	"bytes"
 	"fmt"
-	"strings"
 )
 
 // Warning displays a warning message
@@ -13,26 +12,7 @@ func (log *Logger) Warning(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := warningPrefix(log)
-		log.WarningColor.Fprint(buf, args...)
-		log.WarningColor.Print(buf.String())
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprint(buf, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				// Send the actual message here
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
-		ch <- 1
+		write(warningPrefix, log, log.WarningColor, ch, args...)
 	}(ch)
 	<-ch
 }
@@ -44,26 +24,7 @@ func (log *Logger) Warningf(format string, args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := warningPrefix(log)
-		log.WarningColor.Fprintf(buf, format, args...)
-		// Using Print instead of Printf, since the format string would be taken into account in the Fprintf method
-		log.WarningColor.Print(buf.String())
-
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprintf(buf, format, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
+		writef(warningPrefix, log, log.WarningColor, ch, format, args...)
 	}(ch)
 	<-ch
 }
@@ -75,25 +36,7 @@ func (log *Logger) Warningln(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := warningPrefix(log)
-		log.WarningColor.Fprint(buf, args...)
-		log.WarningColor.Println(buf.String())
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprintln(buf, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				// Send the actual message here
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
+		writeln(warningPrefix, log, log.WarningColor, ch, args...)
 	}(ch)
 	<-ch
 }
@@ -107,6 +50,5 @@ func warningPrefix(log *Logger) (*bytes.Buffer, logMessage) {
 	fmt.Fprint(buf, " ")
 	log.WarningMessageTypeColor.Fprint(buf, logStruct.MessageType)
 	fmt.Fprint(buf, " ")
-
 	return buf, logStruct
 }
