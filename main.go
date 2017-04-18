e482634961da98bd814c28bc2eb33ac1760732c4
Pratheek Adidela <apratheek@unaxiom.com>
2017-04-18 17:54:03 +0530

    Moved code away from main.go into specific files. Added new code.

--- main.go
+++ main.go
@@ -1,51 +1,63 @@
 package ulogger
 
 import (
-	"bytes"
-	"fmt"
 	"time"
+
+	"github.com/fatih/color"
 )
+
+func init() {
+	if remoteFlagString == "true" {
+		remoteFlag = true
+	}
+}
 
 // New returns a logger object
 func New() *Logger {
 	log := new(Logger)
+	// Set info colors here
+	log.InfoColor = color.New(color.FgHiGreen)
+	log.InfoTimeColor = color.New(color.FgHiGreen).Add(color.Underline)
+	log.InfoMessageTypeColor = color.New(color.FgHiGreen)
+	// Set debug colors here
+	log.DebugColor = color.New(color.FgHiWhite).Add(color.BgBlue)
+	log.DebugTimeColor = color.New(color.FgHiWhite)
+	log.DebugMessageTypeColor = color.New(color.FgHiWhite)
+	// Set warning colors here
+	log.WarningColor = color.New(color.FgHiYellow)
+	log.WarningTimeColor = color.New(color.FgHiYellow)
+	log.WarningMessageTypeColor = color.New(color.FgHiYellow)
+	// Set error colors here
+	log.ErrorColor = color.New(color.FgHiBlue)
+	log.ErrorTimeColor = color.New(color.FgHiBlue)
+	log.ErrorMessageTypeColor = color.New(color.FgHiBlue)
+	// Set critical colors here
+	log.CriticalColor = color.New(color.FgHiRed)
+	log.CriticalTimeColor = color.New(color.FgHiRed)
+	log.CriticalMessageTypeColor = color.New(color.FgHiRed)
 	return log
-}
-
-// Info displays a non-critical log message
-func (log *Logger) Info(args interface{}) {
-	// infoColor.Print(time.Now().Format("02-01-06 03:04:05"), "\t", args)
-	// infoColor.Fprintf()
-	infoColor.Print(log.writeToPrint(args))
-}
-
-// Infoln displays a non-critical log message
-func (*Logger) Infoln(args interface{}) {
-	infoColor.Println(args)
-}
-
-// Debug displays a debugging message useful in development environment
-func (*Logger) Debug(args interface{}) {
-	debugColor.Println(args)
-}
-
-// writeToPrint writes the logging message to a string and returns it
-func (log *Logger) writeToPrint(args interface{}) string {
-	buf := new(bytes.Buffer)
-	fmt.Fprint(buf, time.Now().Format("02-01-06 03:04:05"), "\t", args)
-	return buf.String()
-}
-
-// writeToPrintf writes the logging message to a string and returns it
-func (log *Logger) writeToPrintf(format string, args ...interface{}) string {
-	buf := new(bytes.Buffer)
-	fmt.Fprintf(buf, format, time.Now().Format("02-01-06 03:04:05"), "\t", args)
-	return buf.String()
 }
 
 // WithFields adds the passed fields and attaches them to the logging object
 func (log *Logger) WithFields(fields []DisplayField) {
 	for _, field := range fields {
 		log.fieldsToDisplays = append(log.fieldsToDisplays, field)
+	}
+}
+
+// generateTimestamp returns a logMessage along with the time of creation of this log.
+func generateTimestamp() (logMessage, time.Time) {
+	var log logMessage
+	var timestamp = time.Now()
+	log.Timestamp = timestamp.Unix()
+	return log, timestamp
+}
+
+// sendLogMessage sends the logMessage to the remote URL, if the REMOTE_FLAG is set
+func sendLogMessage(log logMessage) {
+	if remoteFlag {
+		// Push the message to the remote URL
+		// Also, attach the organization name and the application name here, before composing a new struct
+
 	}
 }
