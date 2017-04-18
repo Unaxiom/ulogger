495e608d33507619be015d6bfee32fcaf7488778
Pratheek Adidela <apratheek@unaxiom.com>
2017-04-18 20:33:26 +0530

    Redundant/overlapping code from debug.go has been removed.

--- debug.go
+++ debug.go
@@ -3,7 +3,6 @@ package ulogger
 import (
 	"bytes"
 	"fmt"
-	"strings"
 )
 
 // Debug displays a debugging message useful in development environment
@@ -13,26 +12,7 @@ func (log *Logger) Debug(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := debugPrefix(log)
-		log.DebugColor.Fprint(buf, args...)
-		log.DebugColor.Print(buf.String())
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprint(buf, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				// Send the actual message here
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
-		ch <- 1
+		write(debugPrefix, log, log.DebugColor, ch, args...)
 	}(ch)
 	<-ch
 }
@@ -44,26 +24,7 @@ func (log *Logger) Debugf(format string, args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := debugPrefix(log)
-		log.DebugColor.Fprintf(buf, format, args...)
-		// Using Print instead of Printf, since the format string would be taken into account in the Fprintf method
-		log.DebugColor.Print(buf.String())
-
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprintf(buf, format, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
+		writef(debugPrefix, log, log.DebugColor, ch, format, args...)
 	}(ch)
 	<-ch
 }
@@ -75,25 +36,7 @@ func (log *Logger) Debugln(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		// Create the log that needs to be displayed on stdout
-		buf, logStruct := debugPrefix(log)
-		log.DebugColor.Fprint(buf, args...)
-		log.DebugColor.Println(buf.String())
-		go func() {
-			// Create the log message that needs to be sent to the server, only if the RemoteAvailable flag is set
-			if !log.RemoteAvailable {
-				ch <- 1
-				return
-			}
-			buf.Reset()
-			fmt.Fprintln(buf, args...)
-			logStruct.MessageContent = strings.TrimSpace(buf.String())
-			go func() {
-				// Send the actual message here
-				sendLogMessage(logStruct)
-				ch <- 1
-			}()
-		}()
+		writeln(debugPrefix, log, log.DebugColor, ch, args...)
 	}(ch)
 	<-ch
 }
@@ -107,6 +50,5 @@ func debugPrefix(log *Logger) (*bytes.Buffer, logMessage) {
 	fmt.Fprint(buf, " ")
 	log.DebugMessageTypeColor.Fprint(buf, logStruct.MessageType)
 	fmt.Fprint(buf, " ")
-
 	return buf, logStruct
 }
