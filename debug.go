f7bbf683174d60172d5de41de65a86b3e3297263
Pratheek Adidela <apratheek@unaxiom.com>
2017-06-14 21:32:27 +0530

    Runtime parameters are now visible while printing debug logs.

--- debug.go
+++ debug.go
@@ -7,10 +7,14 @@ import (
 
 // Debug displays a debugging message useful in development environment
 func (log *Logger) Debug(args ...interface{}) {
+	var rtParams runtimeParams
+	if log.LineNumber {
+		rtParams.file, rtParams.function, rtParams.line = fetchLocation()
+	}
 	if log.logLevelCode > 1 {
 		if log.RemoteAvailable {
 			// Create the logMessage struct here
-			logStruct, _ := generateTimestamp("DEBUG")
+			logStruct, _ := generateTimestamp("DEBUG", rtParams)
 			ch := make(chan int)
 			go sendLogMessageFromWrite(logStruct, ch, args...)
 			<-ch
@@ -19,17 +23,21 @@ func (log *Logger) Debug(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		write(debugPrefix, log, log.DebugColor, ch, args...)
+		write(debugPrefix, log, log.DebugColor, rtParams, ch, args...)
 	}(ch)
 	<-ch
 }
 
 // Debugf displays a debugging message
 func (log *Logger) Debugf(format string, args ...interface{}) {
+	var rtParams runtimeParams
+	if log.LineNumber {
+		rtParams.file, rtParams.function, rtParams.line = fetchLocation()
+	}
 	if log.logLevelCode > 1 {
 		if log.RemoteAvailable {
 			// Create the logMessage struct here
-			logStruct, _ := generateTimestamp("DEBUG")
+			logStruct, _ := generateTimestamp("DEBUG", rtParams)
 			ch := make(chan int)
 			go sendLogMessageFromWritef(logStruct, ch, format, args...)
 			<-ch
@@ -38,17 +46,21 @@ func (log *Logger) Debugf(format string, args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		writef(debugPrefix, log, log.DebugColor, ch, format, args...)
+		writef(debugPrefix, log, log.DebugColor, rtParams, ch, format, args...)
 	}(ch)
 	<-ch
 }
 
 // Debugln displays a debugging message
 func (log *Logger) Debugln(args ...interface{}) {
+	var rtParams runtimeParams
+	if log.LineNumber {
+		rtParams.file, rtParams.function, rtParams.line = fetchLocation()
+	}
 	if log.logLevelCode > 1 {
 		if log.RemoteAvailable {
 			// Create the logMessage struct here
-			logStruct, _ := generateTimestamp("DEBUG")
+			logStruct, _ := generateTimestamp("DEBUG", rtParams)
 			ch := make(chan int)
 			go sendLogMessageFromWriteln(logStruct, ch, args...)
 			<-ch
@@ -57,7 +69,7 @@ func (log *Logger) Debugln(args ...interface{}) {
 	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		writeln(debugPrefix, log, log.DebugColor, ch, args...)
+		writeln(debugPrefix, log, log.DebugColor, rtParams, ch, args...)
 	}(ch)
 	<-ch
 }
@@ -65,19 +77,28 @@ func (log *Logger) Debugln(args ...interface{}) {
 // DebugDump displays the dump of the variables passed using the go-spew library
 func (log *Logger) DebugDump(args ...interface{}) {
 	// Don't stream this to the remote server
+	var rtParams runtimeParams
+	if log.LineNumber {
+		rtParams.file, rtParams.function, rtParams.line = fetchLocation()
+	}
 	ch := make(chan int)
 	go func(ch chan int) {
-		writeDump(debugPrefix, log, log.DebugColor, ch, args...)
+		writeDump(debugPrefix, log, log.DebugColor, rtParams, ch, args...)
 	}(ch)
 	<-ch
 }
 
 // Returns a string, along with a logMessage after prefixing the timestamp and the type of log
-func debugPrefix(log *Logger) (*bytes.Buffer, logMessage) {
+func debugPrefix(log *Logger, rtParams runtimeParams) (*bytes.Buffer, logMessage) {
 	buf := new(bytes.Buffer)
-	logStruct, timestamp := generateTimestamp("DEBUG")
+	logStruct, timestamp := generateTimestamp("DEBUG", rtParams)
 	logStruct.OrganizationName = log.OrganizationName
 	logStruct.ApplicationName = log.ApplicationName
+	// Print the runtime parameters
+	if log.LineNumber {
+		log.DebugMessageTypeColor.Fprintf(buf, "%s->%s():%d", rtParams.file, rtParams.function, rtParams.line)
+		fmt.Fprint(buf, " ")
+	}
 	log.DebugTimeColor.Fprint(buf, timestamp.Format(timeFormat))
 	fmt.Fprint(buf, " ")
 	log.DebugMessageTypeColor.Fprint(buf, logStruct.MessageType)
